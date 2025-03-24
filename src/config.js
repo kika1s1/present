3508ea8af5636dd4221335391ad8681b2b7dddcb
kika1s1 <tamiratkebede120@gmail.com>
2025-03-24 20:51:34 +0300

    Add dotenv support and update configuration for environment variables

--- src/config.js
+++ src/config.js
@@ -1,20 +1,20 @@
-// src/config.js
+import dotenv from "dotenv"
+dotenv.config({})
 export const config = {
-    telegram: {
-      apiId: process.env.TELEGRAM_API_ID || 27120436,
-      apiHash: process.env.TELEGRAM_API_HASH || '575106948338569871151bf10543488f',
-      stringSession: process.env.TELEGRAM_STRING_SESSION || '1BAAOMTQ5LjE1NC4xNjcuOTEAUFkKLpO9ftf92jmkclQy0Eaj/iKd/1mgwI7zkEa6ENJPNqyBhzCbZ2p2Gr1dC3kgadwq4SPEYjEyHg81nrDi9qiajPELgyI1N9TuTvl4ujjHSViWhpiuwRUL1fgpkmQJ53E9ttAVHfo29z6nMcrieLPVoPvVmvfpAPAbnULXx4EbTxcm6mOwwcng/Hb9DTV0XYgruI+zZxYtRfhq+GOXQqcDFDX0bJsXagebC5FYmoUyLpT6QQ4Dt0egWwyyd0GwNKtiXwP5p9AGYd3CY+u8+nKYIm1adxgmaykrjDP58iU6WpBUF5Vfil/8l8XDTUjEioik4MOOp/1EQXwo2CERQRU=',
-      botUsername: process.env.TELEGRAM_BOT_USERNAME || 'A2SVBouncerBot'
+  telegram: {
+    apiId: Number(process.env.TELEGRAM_API_ID),
+    apiHash: process.env.TELEGRAM_API_HASH,
+    stringSession: process.env.TELEGRAM_STRING_SESSION,
+    botUsername: process.env.TELEGRAM_BOT_USERNAME,
+  },
+  schedule: {
+    monFri: {
+      checkin: process.env.MONFRI_CHECKIN,    // 1:00 PM Monday-Friday
+      checkout: process.env.MONFRI_CHECKOUT   // 8:00 PM Monday-Friday
     },
-    schedule: {
-      monFri: {
-        checkin: '0 13 * * 1-5',    // 1:00 PM Monday-Friday
-        checkout: '0 20 * * 1-5'    // 8:00 PM Monday-Friday
-      },
-      saturday: {
-        checkin: '0 9 * * 6',       // 9:00 AM Saturday
-        checkout: '30 12 * * 6'     // 12:30 PM Saturday
-      }
+    saturday: {
+      checkin: process.env.SATURDAY_CHECKIN,       // 9:00 AM Saturday
+      checkout: process.env.SATURDAY_CHECKOUT     // 12:30 PM Saturday
     }
-  };
-  
+  }
+};
